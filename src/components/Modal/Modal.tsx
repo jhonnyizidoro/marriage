e274965a2730a2ffcadca0aedd07ad5a3a69f354
Jhonny Menarim <jhonny-cwb@hotmail.com>
2025-07-25 12:40:05 -0300

    fix: use client with useSearchParams

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { type FC, type PropsWithChildren, useCallback, useEffect } from 'react'
